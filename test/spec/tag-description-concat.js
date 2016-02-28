62d140654eb33656d6ecae93e72cc825ebd0b3ab
muji <freeformsystems@gmail.com>
2016-02-28 21:50:57 +0800

    Assert on tag description concat.

---
+++ test/spec/tag-description-concat.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect
+  , fs = require('fs')
+  , parse = require('../../index');
+
+describe('cparse:', function() {
+
+  it('should concat short tag description w/ additional lines',
+    function(done) {
+      var source = 'test/fixtures/tag-description-concat.js'
+        , stream = parse.file(source)
+        , expected = ('' + fs.readFileSync(source)).trim()
+        , desc = 'Perform some operation, then try:\n\n var x = \'y\';';
+
+      stream.once('comment', function(comment) {
+        expect(comment.source).to.eql(expected);
+        expect(comment.line).to.eql(1);
+        expect(comment.pos.start).to.eql(1);
+        expect(comment.pos.end).to.eql(5);
+        expect(comment.tags.length).to.eql(1);
+        expect(comment.tags[0].description).to.eql(desc);
+        done();
+      })
+    }
+  );
+
+});
