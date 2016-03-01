da911817f5ef3eb0f860adf393944f35d14911e5
muji <freeformsystems@gmail.com>
2016-03-01 09:35:02 +0800

    Add test case for finish event.

---
+++ test/spec/finish-event.js
@@ -0,0 +1,9 @@
+var parse = require('../../index');
+
+describe('cparse:', function() {
+  it('should emit finish event', function(done) {
+    var source = 'test/fixtures/multiline.js'
+      , stream = parse.load(source);
+    stream.once('finish', done);
+  });
+});
