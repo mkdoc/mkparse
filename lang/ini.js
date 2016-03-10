de8003237810fa6f04e3de36ed5080816280def0
muji <freeformsystems@gmail.com>
2016-03-10 10:35:52 +0800

    Draft ini language pack.

---
+++ lang/ini.js
@@ -0,0 +1,25 @@
+var lang = require('./c');
+
+/**
+ *  Creates an array of language rules for ini files.
+ *
+ *  Recognises continuous blocks of lines beginning with `;`.
+ *
+ *  @function ini
+ *  @param {Object} [opts] processing options.
+ *
+ *  @option {RegExp} mark sub pattern.
+ *  @option {RegExp} trail pattern to strip trailing meta characters.
+ *
+ *  @returns list of language rules.
+ */
+function ini(opts) {
+  opts = opts || {};
+  opts.mark = opts.mark ? opts.mark : /;+/;
+  opts.trail = opts.trail instanceof RegExp
+    ? opts.trail: new RegExp('\\s*' + opts.mark.source + '.*$');
+  // single rule style for this language pack
+  return [lang.single(opts)];
+}
+
+module.exports = ini;
