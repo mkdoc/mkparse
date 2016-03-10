a80c33ce2184a4853a6ba4e14bc2a5a69146f548
muji <freeformsystems@gmail.com>
2016-03-10 13:56:47 +0800

    Support and assert on vim language pack.

---
+++ lang/vim.js
@@ -0,0 +1,25 @@
+var lang = require('./c');
+
+/**
+ *  Creates an array of language rules for vim files.
+ *
+ *  Recognises continuous blocks of lines beginning with `"`.
+ *
+ *  @function vim
+ *  @param {Object} [opts] processing options.
+ *
+ *  @option {RegExp} mark sub pattern.
+ *  @option {RegExp} trail pattern to strip trailing meta characters.
+ *
+ *  @returns list of language rules.
+ */
+function vim(opts) {
+  opts = opts || {};
+  opts.mark = opts.mark ? opts.mark : /"+/;
+  opts.trail = opts.trail instanceof RegExp
+    ? opts.trail: new RegExp('\\s*' + opts.mark.source + '.*$');
+  // single rule style for this language pack
+  return [lang.single(opts)];
+}
+
+module.exports = vim;
---
+++ test/spec/lang/vim.js
@@ -0,0 +1,59 @@
+var expect = require('chai').expect
+  , parse = require('../../../index');
+
+function assert(comments) {
+  expect(comments.length).to.eql(3);
+
+  expect(comments[0].description).to.eql('File description');
+  expect(comments[0].tags.length).to.eql(1);
+  expect(comments[0].tags[0].id).to.eql('file');
+  expect(comments[0].tags[0].name).to.eql('spec.vim');
+
+  expect(comments[1].description).to.eql('Print an error message');
+  expect(comments[1].tags.length).to.eql(2);
+  expect(comments[1].tags[0].id).to.eql('function');
+  expect(comments[1].tags[0].name).to.eql('Error');
+
+  expect(comments[1].tags[1].id).to.eql('param');
+  expect(comments[1].tags[1].name).to.eql('message');
+  expect(comments[1].tags[1].type).to.eql('String');
+
+  expect(comments[2].description).to.eql('Inline comment');
+}
+
+describe('cparse:', function() {
+
+  it('should use vim language', function(done) {
+    var source = 'test/fixtures/lang/spec.vim'
+      , stream = parse.load(source, {rules: require('../../../lang/vim')})
+      , comments = [];
+
+    stream.on('comment', function(comment) {
+      comments.push(comment);
+    })
+
+    stream.on('finish', function() {
+      assert(comments);
+      done();
+    })
+  });
+
+  it('should use vim language w/ options', function(done) {
+    var source = 'test/fixtures/lang/spec.vim'
+      , stream = parse.load(
+          source, {
+            rules: require('../../../lang/vim'),
+            options: {mark: /"+/, trail: /\s*"+.*$/}})
+      , comments = [];
+
+    stream.on('comment', function(comment) {
+      comments.push(comment);
+    })
+
+    stream.on('finish', function() {
+      assert(comments);
+      done();
+    })
+  });
+
+});
