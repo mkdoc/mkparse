266ad5adb91019da5daf4b6ea619a5fda02d2ac2
muji <freeformsystems@gmail.com>
2016-03-12 07:09:52 +0800

    Add block test fixture.

---
+++ test/spec/block.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect
+  , fs = require('fs')
+  , parse = require('../../index');
+
+describe('cparse:', function() {
+
+  it('should parse single-line comment block', function(done) {
+    var source = 'test/fixtures/block.js'
+      , stream = parse.load(source)
+      , expected = ('' + fs.readFileSync(source)).trim();
+
+    stream.once('comment', function(comment) {
+      expect(comment.source).to.eql(expected);
+      expect(comment.description).to.eql('This is a multi-line \ndescription');
+      expect(comment.line).to.eql(1);
+      expect(comment.pos.start).to.eql(1);
+      expect(comment.pos.end).to.eql(2);
+      expect(comment.tags).to.eql([]);
+      done();
+    })
+  });
+
+});
