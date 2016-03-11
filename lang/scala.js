4d7741c3782788304754ce8b4bf0360e81fb7305
muji <freeformsystems@gmail.com>
2016-03-11 20:45:45 +0800

    Define scala language and fixture.

---
+++ lang/scala.js
@@ -0,0 +1,14 @@
+/**
+ *  Creates an array of language rules for scala files.
+ *
+ *  Recognises continuous lines with `//` comments and terminated 
+ *  multi-line comments starting with `/**`.
+ *
+ *  See the [default settings](#defaults).
+ *
+ *  @function scala
+ *  @param {Object} [opts] processing options.
+ *
+ *  @returns list of language rules.
+ */
+module.exports = require('./index.js');
---
+++ test/fixtures/lang/spec.scala
@@ -0,0 +1,17 @@
+/**
+ * File description
+ * 
+ * @file spec.scala
+ */
+object spec {
+
+  /**
+   * Print an error message
+   * 
+   * @function error
+   * @param {String} message Error message
+   */
+   def error(message: String) {
+      println(message) // Inline comment
+   }
+}
