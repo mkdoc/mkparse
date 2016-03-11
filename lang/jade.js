d2ca444fa381af1b408cfcbcccbbec9ab9a569f5
muji <freeformsystems@gmail.com>
2016-03-11 14:04:51 +0800

    Support and assert on jade language.

---
+++ lang/jade.js
@@ -0,0 +1,20 @@
+var lang = require('./index.js');
+
+/**
+ *  Creates an array of language rules for jade files.
+ *
+ *  Recognises continuous blocks starting with `//-` or `//`.
+ *
+ *  @function jade
+ *  @param {Object} [opts] processing options.
+ *
+ *  @returns list of language rules.
+ */
+function jade(opts) {
+  opts = opts || {};
+  opts.mark = /\/\/-/;
+  // longer pattern should come first
+  return [lang.single(opts), lang.single(opts.single)];
+}
+
+module.exports = jade;
