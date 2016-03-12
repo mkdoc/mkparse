1e85d40227920faa77d9ac3b4a39aef16b3a0e98
muji <freeformsystems@gmail.com>
2016-03-12 16:28:34 +0800

    Support and assert on less language.

---
+++ lang/less.js
@@ -0,0 +1,22 @@
+var lang = require('./css');
+
+/**
+ *  Creates an array of language rules for less files.
+ *
+ *  Recognises terminated multi-line comments starting with `/*` and 
+ *  continuous blocks of single-line comments beginning with `//`.
+ *
+ *  See the [css language](#css).
+ *
+ *  @function less
+ *  @param {Object} [opts] processing options.
+ *
+ *  @returns list of language rules.
+ */
+function less(opts) {
+  opts = opts || {};
+  // enable single line comments
+  opts.single = {};
+  return lang(opts);
+}
+module.exports = less;
