00755681551407001241524ac8878f5cd89cf1f4
muji <freeformsystems@gmail.com>
2016-03-12 18:35:43 +0800

    Support and assert on processing (java).

---
+++ lang/processing.js
@@ -0,0 +1,14 @@
+/**
+ *  Creates an array of language rules for processing files.
+ *
+ *  Recognises continuous lines with `//` comments and terminated 
+ *  multi-line comments starting with `/**`.
+ *
+ *  See the [default settings](#defaults).
+ *
+ *  @function processing
+ *  @param {Object} [opts] processing options.
+ *
+ *  @returns list of language rules.
+ */
+module.exports = require('./index.js');
---
+++ test/fixtures/lang/spec.processing.java
@@ -0,0 +1,17 @@
+/**
+ * File description
+ * 
+ * @file spec.processing.java
+ */
+
+class ErrorMessage {
+  /**
+   * Print an error message
+   * 
+   * @function error
+   * @param {String} message Error message
+   */
+  void error(String message) {
+    // Inline comment
+  }
+}
